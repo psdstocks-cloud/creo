e552458d0b637fb9d9eb96ec9839034d0ac314f7
PSD <psdstockss@gmail.com>
2025-10-08 13:35:18 +0300

    Fix next-intl.config.js to use CommonJS syntax for Netlify compatibility

--- next-intl.config.js
+++ next-intl.config.js
@@ -1,5 +1,5 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+const createNextIntlPlugin = require('next-intl/plugin');
  
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
  
-export default withNextIntl;
+module.exports = withNextIntl;
